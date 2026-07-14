d36f3d39271fc79452e0ab78e06e553e3e7a00b1
johncEV <john.collins@emptyvessel.io>
2026-07-14 21:46:17 +1000

    Changed article scenes, added simple inventory script

---
+++ src/PlayerSingleton.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class PlayerSingleton : Node
+{
+    public static PlayerInventory Inventory {get; set;}
+}
--- src/backend/ArticleNode.cs
+++ src/backend/ArticleNode.cs
@@ -6,7 +6,7 @@ using EverythingKnownToMan.backend;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
-public partial class ArticleNode : Node
+public partial class ArticleNode : Control
 {
     public WikiArticle WikiArticle;
     
@@ -16,21 +16,38 @@ public partial class ArticleNode : Node
     [Export] private Label DescLabel;
     [Export] private Label ParagraphLabel;
     [Export] private Label TagsLabel;
+    [Export] private Button ClickableBackground;
     [Export] private bool bLoadFromDisk = false;
     [Export] private string DiskFileName;
 
     public override void _Ready()
     {
         base._Ready();
+        if (ClickableBackground != null)
+        {
+            GD.Print("ClickableBackground bound");
+            ClickableBackground.Pressed += OnClickableBackgroundOnPressed;
+        }
         if (bLoadFromDisk && !string.IsNullOrEmpty(DiskFileName))
         {
             LoadFromDisk(DiskFileName);
         }
     }
 
+    private void OnClickableBackgroundOnPressed()
+    {
+        GD.Print("OnClickableBackgroundOnPressed: " + (WikiArticle != null ? WikiArticle.Title : "Empty"));
+        PlayerSingleton.Inventory.SwapArticle(this);
+    }
+
     public void LoadArticle(WikiArticle wikiArticle)
     {
         WikiArticle = wikiArticle;
+        if (wikiArticle == null)
+        {
+            Hide();
+        }
+        
         if (WikiArticle.Image != null)
         {
             var texture = ImageTexture.CreateFromImage(ImageFromWikiImage(wikiArticle.Image));
@@ -42,16 +59,22 @@ public partial class ArticleNode : Node
             ArticleTexture.Texture = null;
         }
         
-        TitleLabel.Text = wikiArticle.Title;
-        DescLabel.Text = wikiArticle.Description;
-        ParagraphLabel.Text = wikiArticle.Paragraph;
-        if (WikiArticle.Tags != null)
+        if (TitleLabel != null) 
+            TitleLabel.Text = wikiArticle.Title;
+        if (DescLabel != null) 
+            DescLabel.Text = wikiArticle.Description;
+        if (ParagraphLabel != null) 
+            ParagraphLabel.Text = wikiArticle.Paragraph;
+        if (TagsLabel != null)
         {
-            TagsLabel.Text = $"Tags: {string.Join(", ", WikiArticle.Tags)}";
-        }
-        else
-        {
-            TagsLabel.Text = "No Tags";
+            if (WikiArticle.Tags != null)
+            {
+                TagsLabel.Text = $"Tags: {string.Join(", ", WikiArticle.Tags)}";
+            }
+            else
+            {
+                TagsLabel.Text = "No Tags";
+            }
         }
     }
 
