0ad6667b9836a87f48f381f70b0512b18af610e4
Scaroficous <59405072+Scaroficous@users.noreply.github.com>
2026-07-16 01:48:01 +1000

    All obstacles and NPCs placed in the levels

--- Oscar's Scripts (bad)/character_follow_path.gd
+++ Oscar's Scripts (bad)/character_follow_path.gd
@@ -7,6 +7,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 		#move along the path if nothing is in the road
-	if (!(Input.get_axis("ui_left", "ui_right") > 0 && get_node("ResearcherPlaceholder/RayCast2D").is_colliding())):
+	if (!(get_node("ResearcherPlaceholder/RayCast2D").is_colliding())):
 		progress_ratio += 0.015 * delta * Input.get_axis("ui_left", "ui_right")
 	
--- src/Spawner.cs
+++ src/Spawner.cs
@@ -4,45 +4,49 @@ using EverythingKnownToMan.backend;
 
 public partial class Spawner : Node
 {
-    [Export] public string[] tags;
-    [Export] public int numArticles;
-    [Export] public string articleScenePath;
-    private PackedScene articleSceneTemplate;
-    [Export] public float maxRadius;
-    [Export] public float minRadius;
+	[Export] public string[] tags;
+	[Export] public int numArticles;
+	[Export] public string articleScenePath;
+	private PackedScene articleSceneTemplate;
+	[Export] private bool spawnOnReady = false;
+	[Export] public float maxRadius;
+	[Export] public float minRadius;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        articleSceneTemplate = GD.Load<PackedScene>(articleScenePath);
-        //SpawnArticles(numArticles);
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		articleSceneTemplate = GD.Load<PackedScene>(articleScenePath);
+		if (spawnOnReady)
+		{
+			SpawnArticles(numArticles);
+		}
+	}
 
-    public void SpawnArticles(int numArticles_)
-    {
-        for (int i=0; i<numArticles_; i++)
-            SpawnArticle();
-    }
-    
-    public void SpawnArticles() { SpawnArticles(numArticles); }
+	public void SpawnArticles(int numArticles_)
+	{
+		for (int i=0; i<numArticles_; i++)
+			SpawnArticle();
+	}
+	
+	public void SpawnArticles() { SpawnArticles(numArticles); }
 
-    public void SpawnArticle()
-    {
-        WikiArticle article = WikiDatabase.Instance.RequestRandomArticleOfTag(tags);
-        if (article == null)
-        {
-            return;
-        }
-        ArticleNode2D articleNode2D = articleSceneTemplate.Instantiate<ArticleNode2D>();
-        AddChild(articleNode2D);
-        articleNode2D.ArticleNode.LoadArticle(article);
-        articleNode2D.Position = GetRandomOffset();
-    }
+	public void SpawnArticle()
+	{
+		WikiArticle article = WikiDatabase.Instance.RequestRandomArticleOfTag(tags);
+		if (article == null)
+		{
+			return;
+		}
+		ArticleNode2D articleNode2D = articleSceneTemplate.Instantiate<ArticleNode2D>();
+		AddChild(articleNode2D);
+		articleNode2D.ArticleNode.LoadArticle(article);
+		articleNode2D.Position = GetRandomOffset();
+	}
 
-    public Vector2 GetRandomOffset()
-    {
-        float distance = new Random().NextSingle() * (maxRadius - minRadius) + minRadius;
-        float angle = new Random().NextSingle() * MathF.PI * 2;
-        return new Vector2(MathF.Cos(angle) * distance, MathF.Sin(angle) * distance);
-    }
+	public Vector2 GetRandomOffset()
+	{
+		float distance = new Random().NextSingle() * (maxRadius - minRadius) + minRadius;
+		float angle = new Random().NextSingle() * MathF.PI * 2;
+		return new Vector2(MathF.Cos(angle) * distance, MathF.Sin(angle) * distance);
+	}
 }
